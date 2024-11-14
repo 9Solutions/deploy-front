3a80a05c0ee0c0edd53ee02ac5fd5a2eb3da43bc
Thigos Rodrigues <67590378+Thigos@users.noreply.github.com>
2024-11-14 17:47:48 -0300

    Add files via upload

---
+++ deploy-front/deploy-dash/entrypoint.dashboard.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+# Diretório onde o código será clonado
+REPO_DIR="dashboard/"
+
+# Verifica se o repositório já foi clonado
+if [ -d "dashboard/.git" ]; then
+    echo "Atualizando repositório existente..."
+    cd "$REPO_DIR"
+    git pull origin $GIT_BRANCH
+else
+    echo "Clonando repositório..."
+    git clone -b $GIT_BRANCH $GIT_REPO
+    cd "$REPO_DIR"
+fi
+
+nom i
+
+npm start
---
+++ deploy-front/deploy-site-doacoes/entrypoint.site.doacoes.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+# Diretório onde o código será clonado
+REPO_DIR="Site-Doacoes/"
+
+# Verifica se o repositório já foi clonado
+if [ -d "Site-Doacoes/.git" ]; then
+    echo "Atualizando repositório existente..."
+    cd "$REPO_DIR"
+    git pull origin $GIT_BRANCH
+else
+    echo "Clonando repositório..."
+    git clone -b $GIT_BRANCH $GIT_REPO
+    cd "$REPO_DIR"
+fi
+
+nom i
+
+npm start
